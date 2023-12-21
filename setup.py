e68f09c2b80c33ff19cd4b1da0c2499f02a245cb
CoCFire <109904491+CoC-Fire@users.noreply.github.com>
2023-12-20 21:32:16 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cocfire-url-shortener-api',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+)
