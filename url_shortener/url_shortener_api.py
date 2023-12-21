5186db5254a615b18c5714289771e8a37ca70367
CoCFire <109904491+CoC-Fire@users.noreply.github.com>
2023-12-20 21:29:35 -0800

    Create url_shortener_api.py

---
+++ url_shortener/url_shortener_api.py
@@ -0,0 +1,50 @@
+# url_shortener_api.py
+
+import requests
+
+class RateLimitError(Exception):
+    """Raised when the ratelimit is exceeded"""
+    pass
+
+class InvalidRequestError(Exception):
+    """Raised when an invalid url is passed"""
+    pass
+
+def shorten_url(url: str):
+    """
+    Shortens a URL
+    --------------
+
+    Args:
+    -----
+
+    `url`:
+        Type: `str`
+        Required: `True`
+
+
+    Raises:
+    -------
+
+    `RateLimitError`:
+        Reasons: `Raised when the ratelimit is exceeded`
+    
+    `InvalidRequestError`:
+        Reasons: `Raised when an invalid url is passed`
+    """
+    headers = {
+        "URL": url,
+        "Content-Type": "application/json"
+    }
+    request = requests.get(
+        "https://cocfire.xyz/api/shortener", 
+        headers = headers
+    )
+    if request.status_code == 200:
+        return request.text
+    elif request.status_code == 429:
+        raise RateLimitError
+    elif request.status_code == 400:
+        raise InvalidRequestError
+
+__all__ = ['RateLimitError', 'InvalidRequestError', 'shorten_url']
