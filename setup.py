4716e8a05b23f7c49e7f23014efaae72a0fedd44
CoCFire <109904491+CoC-Fire@users.noreply.github.com>
2023-12-20 22:58:43 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('README.rst', 'r') as file:
+    long_description = file.read()
+
 setup(
     name='cocfire-url-shortener-api',
-    version='0.0.1',
+    version='0.0.5',
+    long_description = long_description,
+    long_description_content_type='text/x-rst',
     packages=find_packages(),
     install_requires=[
         'requests',
